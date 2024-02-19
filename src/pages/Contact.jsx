ab4e5b0054a311ec3e2e555a7e1307a369ee9538
Muhammad Saad <msaad.bscs22seecs@seecs.edu.pk>
2024-02-19 18:39:23 -0500

    contact updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -31,9 +31,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Saad",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "saadydev@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
@@ -64,7 +64,7 @@ const Contact = () => {
 
           showAlert({
             show: true,
-            text: "I didn't receive your message 😢",
+            text: "I didn't receive your message 😢. Try again",
             type: "danger",
           });
         }
